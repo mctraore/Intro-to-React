8cee7b12cda5ce58470e8616d98ecfdf46b756d2
mctraore <marieclairetraore@gmail.com>
2021-10-21 00:43:20 -0400

    Add header and footer components

---
+++ travel-list/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+           <h3>A Blog by Marie-Claire</h3> 
+           <p>I was inspired to create this blog to inspire myself and others to see the world!</p>
+        </div>
+    )
+}
+
+export default Footer
